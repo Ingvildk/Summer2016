a42d37e81d1b4eff7b22fdf8909c0bf1b92a5965
Ingvild <ingvild_kongsgaard@hotmail.com>
2016-07-11 16:20:59 -0700

    The input box now goes blank after added an element to a list

--- src/Searchbox.js
+++ src/Searchbox.js
@@ -4,10 +4,20 @@ export default class Searchbox extends React.Component {
 	constructor() {
 		super();
 		this.state = {
-			data: []
+			data: [],
+			value: ''
 		};
 		this.submitHandler = this.submitHandler.bind(this);
+		this.changeHandler = this.changeHandler.bind(this);
 
+	}
+
+	changeHandler() {
+		let input = this.refs.myInput;
+		let inputValue = input.value;
+		let stateData = this.state;	
+		stateData.value = inputValue;
+		this.setState({stateData});	
 	}
 
 	submitHandler() {
@@ -18,17 +28,19 @@ export default class Searchbox extends React.Component {
 		console.log(inputValue);
 		let stateInfo = this.state;
 		stateInfo.data.push(inputValue);
+		stateInfo.value = '';
 		this.setState({stateInfo});
 	}
 
 	render() {
 		let count = 0;
 		let elements = this.state.data.map( el =>  (<li key={count++}> {el} </li>));
+		let inputValue = this.state.value;
 		console.log("RENDER---------RENDER");
 		console.log(elements);
 		return (
 			<div>
-				<input type="text" ref="myInput"/>
+				<input type="text" ref="myInput" value={inputValue} onChange={this.changeHandler}/>
 				<br/>
 				<button type="submit" value="Submit" onClick={this.submitHandler}> </button>
 				<ol> {elements} </ol>
