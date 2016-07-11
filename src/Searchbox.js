2220e9965deb7627347a008578d2793f5673237a
Ingvild <ingvild_kongsgaard@hotmail.com>
2016-07-11 14:37:13 -0700

    Got the submit handler to work. Now I need to bind what user types in and render it into the document

--- src/Searchbox.js
+++ src/Searchbox.js
@@ -4,7 +4,7 @@ export default class Searchbox extends React.Component {
 	constructor() {
 		super();
 		this.state = {
-			data: "heisann"
+			data: []
 		};
 		this.submitHandler = this.submitHandler.bind(this);
 
@@ -12,14 +12,19 @@ export default class Searchbox extends React.Component {
 
 	submitHandler() {
 		console.log("submitHandler is running");
-		this.setState({data: "California"});
+		let stateInfo = this.state;
+		stateInfo.data.push("California");
+		this.setState({stateInfo});
 	}
 
 	render() {
+		let elements = this.state.data.map( el => (<li> {el} </li>) );
+		console.log("RENDER---------RENDER");
+		console.log(elements);
 		return (
 			<div>
-				<button type="submit" value="Submit" onClick={t65bfnthis.submitHandler}> </button>
-				<p> {this.state.data} </p>
+				<button type="submit" value="Submit" onClick={this.submitHandler}> </button>
+				<ol> {elements} </ol>
 			</div>
 			);
 	}
