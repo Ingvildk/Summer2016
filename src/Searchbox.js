a5d05d10deed6ed55b3bab451106a4def2b5aa5e
Ingvild <ingvild_kongsgaard@hotmail.com>
2016-07-12 15:32:11 -0700

    The Searchbox is now working. The code is not that pretty, with a lot of commented out ines which needs to be deletet.

--- src/Searchbox.js
+++ src/Searchbox.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import _ from 'underscore';
 export default class Searchbox extends React.Component {
 	
 	constructor() {
 		super();
 		this.state = {
-			data: [{name:"Ingvild", age:22},{name:"Erik", age:61},{name:"Wenke", age:60},{name: "Kyrre", age: 30},{name: "Haavard", age: 32}]
+			data: [{name:"Ingvild", age:22},{name:"Erik", age:61},{name:"Wenke", age:60},{name: "Kyrre", age: 30},{name: "Haavard", age: 32}],
+			searchFilter:[]
 		};
 //		this.submitHandler = this.submitHandler.bind(this);
 		this.changeHandler = this.changeHandler.bind(this);
@@ -15,15 +17,28 @@ export default class Searchbox extends React.Component {
 	checkSearch() {
 		let elements = this.state.data;
 
-		return true; 
+		//return true; 
 	}	
 
 	changeHandler() {
 
 		let input = this.refs.myInput;
 		let inputValue = input.value;
-		inputValue.filter(this.checkSearch());
-		console.log("--End of changeHandler--");
+		let dataValue = this.state.data;
+		let newSearchFilter = [];
+		dataValue.map(element => {
+		let el = element.name.toLowerCase();	
+		if (el.includes(inputValue)) {
+			newSearchFilter.push(element);			
+		}
+		})
+		//this.state.data.filter(checkSearch);
+		this.state.searchFilter = newSearchFilter;
+		let newState = this.state
+		this.setState({newState});
+
+		console.log("--searchFilter--");
+		console.log(this.state.searchFilter);
 		/*
 		let stateData = this.state;	
 		stateData.value = inputValue;
@@ -46,10 +61,8 @@ export default class Searchbox extends React.Component {
 	render() {
 		let count = 0;
 		//let elements = this.state.data.map( el =>  (<li key={count++}> {el.name} </li>));
-		let elements = this.state.data.map(el=> <li key={count++}> {el.name} </li>);
+		let elements = this.state.searchFilter.map(el=> <li key={count++}> {el.name} </li>);
 		let inputValue = this.state.value;
-		console.log("RENDER---------RENDER");
-		console.log(elements);
 		return (
 			<div>
 				<input type="text" ref="myInput" onChange={this.changeHandler}/>
