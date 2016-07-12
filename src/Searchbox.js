0f31730f93b1f7bf5562ab0034a701723cbf51e7
Ingvild <ingvild_kongsgaard@hotmail.com>
2016-07-12 15:37:09 -0700

    Cleaned up code

--- src/Searchbox.js
+++ src/Searchbox.js
@@ -8,17 +8,9 @@ export default class Searchbox extends React.Component {
 			data: [{name:"Ingvild", age:22},{name:"Erik", age:61},{name:"Wenke", age:60},{name: "Kyrre", age: 30},{name: "Haavard", age: 32}],
 			searchFilter:[]
 		};
-//		this.submitHandler = this.submitHandler.bind(this);
 		this.changeHandler = this.changeHandler.bind(this);
-		this.checkSearch = this.checkSearch.bind(this);
 
 	}
-
-	checkSearch() {
-		let elements = this.state.data;
-
-		//return true; 
-	}	
 
 	changeHandler() {
 
@@ -32,35 +24,17 @@ export default class Searchbox extends React.Component {
 			newSearchFilter.push(element);			
 		}
 		})
-		//this.state.data.filter(checkSearch);
 		this.state.searchFilter = newSearchFilter;
 		let newState = this.state
 		this.setState({newState});
 
 		console.log("--searchFilter--");
 		console.log(this.state.searchFilter);
-		/*
-		let stateData = this.state;	
-		stateData.value = inputValue;
-		this.setState({stateData});	*/
+
 	}
 
-/*
-	submitHandler() {
-		console.log("submitHandler is running");
-		let input = this.refs.myInput;
-		let inputValue = input.value;
-		console.log("-----submitHandler-----");
-		console.log(inputValue);
-		let stateInfo = this.state;
-		stateInfo.data.push(inputValue);
-		stateInfo.value = '';
-		this.setState({stateInfo});
-	}
-*/
 	render() {
 		let count = 0;
-		//let elements = this.state.data.map( el =>  (<li key={count++}> {el.name} </li>));
 		let elements = this.state.searchFilter.map(el=> <li key={count++}> {el.name} </li>);
 		let inputValue = this.state.value;
 		return (
