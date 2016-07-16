f8caa5800da647bc5f012a916fb31ecbec49af46
Ingvild <ingvild_kongsgaard@hotmail.com>
2016-07-16 14:32:43 -0700

    Implementing the redux system into my app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Client from './client';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const reducer = function(state, action) {
+	return state;
+}
+
+const store = createStore(reducer, 0); 
+
+store.subscribe(() => {
+	console.log("store changes", store.getState())
+})
+
+store.dispatch({type: "nothing special, but all actions need a type keyword"});
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App> </App> 
+	</Provider>, document.getElementById('root')
+	);
